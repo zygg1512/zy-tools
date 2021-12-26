dc2178cb3b8edd08f06c13147581e23b8d1ebce7
wangquanwu <wangquanwu@smzdm.com>
2021-12-26 16:40:23 +0800

    build: 修改commitlinit.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', ['feature', 'update', 'fixbug', 'refactor', 'optimize', 'style', 'docs', 'chore']],
+        'type-enum': [2, 'always', ['feature', 'update', 'fix', 'build', 'docs']],
         'type-case': [0],
         'type-empty': [0],
         'scope-empty': [0],
